58cba9ed857c913ecd23de95715193c8b5309c27
p.strelchenko <p.strelchenko@hh.ru>
2024-08-28 16:03:13 +0500

    04 -- Creates new AnAction

---
+++ src/main/kotlin/com/github/pstrelchenko/podlodkacrew2024plugintalk/actions/CreatePageObjectAction.kt
@@ -0,0 +1,12 @@
+package com.github.pstrelchenko.podlodkacrew2024plugintalk.actions
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+
+class CreatePageObjectAction : AnAction() {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        // TODO: insert action logic here
+    }
+
+}

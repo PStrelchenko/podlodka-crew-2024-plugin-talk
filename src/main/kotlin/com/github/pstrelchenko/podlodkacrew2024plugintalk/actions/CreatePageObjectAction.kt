9553166621cb126fd15edcec7611a11d8ce8a649
p.strelchenko <p.strelchenko@hh.ru>
2024-08-28 16:06:19 +0500

    05 -- Migrates to CodeInsightAction and adds context validation

--- src/main/kotlin/com/github/pstrelchenko/podlodkacrew2024plugintalk/actions/CreatePageObjectAction.kt
+++ src/main/kotlin/com/github/pstrelchenko/podlodkacrew2024plugintalk/actions/CreatePageObjectAction.kt
@@ -1,12 +1,42 @@
 package com.github.pstrelchenko.podlodkacrew2024plugintalk.actions
 
-import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.codeInsight.CodeInsightActionHandler
+import com.intellij.codeInsight.actions.CodeInsightAction
+import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiFile
+import com.intellij.psi.util.PsiTreeUtil
+import com.intellij.psi.util.PsiUtilBase
+import org.jetbrains.kotlin.psi.KtFile
+import org.jetbrains.kotlin.psi.KtNamedFunction
 
-class CreatePageObjectAction : AnAction() {
+/**
+ * Algorithm:
+ *
+ * 1. Context
+ * 2. Data
+ * 3. Generate
+ */
+class CreatePageObjectAction : CodeInsightAction(), CodeInsightActionHandler {
 
-    override fun actionPerformed(e: AnActionEvent) {
-        // TODO: insert action logic here
+    override fun getHandler(): CodeInsightActionHandler {
+        return this
     }
+
+    override fun isValidForFile(project: Project, editor: Editor, file: PsiFile): Boolean {
+        if (file !is KtFile) {
+            return false
+        }
+
+        val psiElement = PsiUtilBase.getElementAtCaret(editor) ?: return false
+        val composableFunction = PsiTreeUtil.getParentOfType(psiElement, KtNamedFunction::class.java) ?: return false
+
+        return composableFunction.annotationEntries.map { it.text }.any { it.contains("Composable") }
+    }
+
+    override fun invoke(project: Project, editor: Editor, psiFile: PsiFile) {
+        // TODO
+    }
+
 
 }

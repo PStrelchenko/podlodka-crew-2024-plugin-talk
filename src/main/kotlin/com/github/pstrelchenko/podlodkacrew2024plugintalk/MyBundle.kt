63fe608049e4bb0cc0c18485eedcb8d281d9671c
GitHub Action <action@github.com>
2024-08-28 10:48:50 +0000

    Template cleanup

--- src/main/kotlin/com/github/pstrelchenko/podlodkacrew2024plugintalk/MyBundle.kt
+++ src/main/kotlin/com/github/pstrelchenko/podlodkacrew2024plugintalk/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.pstrelchenko.podlodkacrew2024plugintalk
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/pstrelchenko/podlodkacrew2024plugintalk/toolWindow/MyToolWindowFactory.kt
+++ src/main/kotlin/com/github/pstrelchenko/podlodkacrew2024plugintalk/toolWindow/MyToolWindowFactory.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.toolWindow
+package com.github.pstrelchenko.podlodkacrew2024plugintalk.toolWindow
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.diagnostic.thisLogger
@@ -8,8 +8,8 @@ import com.intellij.openapi.wm.ToolWindowFactory
 import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBPanel
 import com.intellij.ui.content.ContentFactory
-import org.jetbrains.plugins.template.MyBundle
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.pstrelchenko.podlodkacrew2024plugintalk.MyBundle
+import com.github.pstrelchenko.podlodkacrew2024plugintalk.services.MyProjectService
 import javax.swing.JButton
 
 
